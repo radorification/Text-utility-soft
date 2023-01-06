1eb0131e4b79379d560132dfc25f05055a84d825
radorification <kumarankur131@gmail.com>
2023-01-06 19:28:20 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './components/Navbar';
+import TextArea from './components/TextArea';
+
+function App() {
+  return (
+    <>
+<Navbar title="TextUtils3" ></Navbar>
+<TextArea></TextArea>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TextArea.js
@@ -0,0 +1,40 @@
+import React,{ useState } from 'react'
+import { Container } from 'react-bootstrap'
+
+export default function TextArea() {
+ const [text, setText] = useState("Enter text here!");
+ const upClick= () => {
+     console.log("Uppercase initialized");
+     let newText= text.toUpperCase();
+     setText(newText);
+ }
+ const downClick= () => {
+    console.log("Lowercase initialized");
+    let newText= text.toLowerCase();
+    setText(newText);
+ }
+ const spaceClick= () => {
+    console.log("Spaces removed");
+    let newText= text.trim().split(/ +/).join('');
+    setText(newText);
+ }
+
+ const handleonChange= (event) => {
+        console.log("On change"+{text})
+        setText(event.target.value)
+ }
+  return ( <>
+    <Container>
+        <div>TextArea</div>
+        <div className="mb-3">
+            <textarea className="form-control" id="myBox" rows="8" onChange={handleonChange} value={text}></textarea>
+            <br></br>
+            <button className="btn btn-primary" onClick={upClick}>To Uppercase</button>&ensp;
+            <button className="btn btn-primary" onClick={downClick}>To Lowercase</button>&ensp;
+            <button className="btn btn-primary" onClick={spaceClick}>Remove Spaces</button>&ensp;
+
+        </div>
+    </Container>
+    </>
+  )
+}
