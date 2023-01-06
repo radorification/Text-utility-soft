799910b79c1511244af2b66bce534da78fcd43b1
radorification <kumarankur131@gmail.com>
2023-01-06 20:59:22 +0530

    Summarizer added

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -15,7 +15,7 @@ export default function TextArea() {
  }
  const spaceClick= () => {
     console.log("Spaces removed");
-    let newText= text.trim().split(/ +/).join('');
+    let newText= text.trim().split(/ +/).join('');//Important concept
     setText(newText);
  }
 
@@ -23,18 +23,23 @@ export default function TextArea() {
         console.log("On change"+{text})
         setText(event.target.value)
  }
-  return ( <>
-    <Container>
-        <div>TextArea</div>
-        <div className="mb-3">
-            <textarea className="form-control" id="myBox" rows="8" onChange={handleonChange} value={text}></textarea>
-            <br></br>
-            <button className="btn btn-primary" onClick={upClick}>To Uppercase</button>&ensp;
-            <button className="btn btn-primary" onClick={downClick}>To Lowercase</button>&ensp;
-            <button className="btn btn-primary" onClick={spaceClick}>Remove Spaces</button>&ensp;
-
+  return (<>
+        <Container>
+            <div>TextArea</div>
+            <div className="mb-3">
+                <textarea className="form-control" id="myBox" rows="8" onChange={handleonChange} value={text}></textarea>
+                <br></br>
+                <button className="btn btn-primary" onClick={upClick}>To Uppercase</button>&ensp;
+                <button className="btn btn-primary" onClick={downClick}>To Lowercase</button>&ensp;
+                <button className="btn btn-primary" onClick={spaceClick}>Remove Spaces</button>&ensp;
+            </div>
+        </Container>   
+        <div className="container">
+            <h1>Your text summary:</h1>
+            <p>
+            {text.length} words and {text.split(" ").length} charcters.
+            </p>
         </div>
-    </Container>
     </>
   )
 }
